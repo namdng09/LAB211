0e1e9c3c89b9c63af7c92b89b24533677bad2996
Pham Nam Duong <namduong090104@gmail.com>
2023-11-24 09:50:53 +0700

    Upload J1.S.P0001

---
+++ J1.S.P0001/src/BubbleSort.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DELL VOSTRO 5630
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
