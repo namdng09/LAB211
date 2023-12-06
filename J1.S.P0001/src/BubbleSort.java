b6e1f45e13b2c3570c48466fc11ccaa67b37b707
Pham Nam Duong <namduong090104@gmail.com>
2023-12-06 17:49:34 +0700

    add J1.S.P0001

--- J1.S.P0001/src/BubbleSort.java
+++ J1.S.P0001/src/BubbleSort.java
@@ -14,76 +14,85 @@ import java.util.Scanner;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        int size_array = getInt("Enter a positive decimal number: ");
-        int[] array = createArray(size_array);
-        System.out.print("Unsort array: ");
+        int num = getNumber("Enter the number of array: ");
+        int[] array = createArray(num);
+        System.out.print("Unsorted array: ");
         display(array);
         System.out.println();
         bubbleSort(array);
+        System.out.println();
         System.out.print("Sorted array: ");
         display(array);
     }
     
-    public static void bubbleSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j+1]) {
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+    public static int getNumber(String str) {
+        Scanner sc = new Scanner(System.in);
+        double num = 0;
+        
+        do {          
+            System.out.print(str);
+            try {
+                num = sc.nextDouble();
+                if ((int)num <= 0) {
+                    System.out.println("Invalid Number!!");
                 }
+            } catch (Exception e) {
+                System.out.println("Invalid Number!!");
+                sc.nextLine();
             }
+        } while (!isPosDecimalNum("" + num));
+        
+        return (int) num;
+    }
+    
+    public static boolean isPosDecimalNum (String input) {
+        try {
+            double number = Double.parseDouble(input);
+            return (int) number > 0;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
     
-    public static void swap(int x, int y) {
-        
-    }
-    
     public static int[] createArray (int size_array) {
-        int[] array = new int[size_array];
         Random rd = new Random();
+        int[] array = new int[size_array];
         for (int i = 0; i < array.length; i++) {
             array[i] = rd.nextInt(100);
         }
         return array;
-    } 
+    }
     
-    public static void display(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
+    public static void display (int[] array) {
+        for (int i = 0; i < array.length; i++) {
             if (i == 0) {
-                System.out.print("[" + arr[i] + ", ");
-            } else if (i == arr.length - 1) {
-                System.out.print(arr[i] + "]");
-            }else {
-                System.out.print(arr[i] + ", ");
+                System.out.print("[" + array[i] + ", ");
+            } else if (i == array.length - 1) {
+                System.out.print(array[i] + "]");
+            } else {
+                System.out.print(array[i] + ", ");
             }
         }
     }
     
-    public static int getInt(String string) {
-        double number = 0;
-        Scanner sc = new Scanner(System.in);
-        do {            
-            System.out.print(string);
-            try {
-                number = sc.nextDouble();
-            } catch (Exception e) {
-                System.out.println("Invalid number!!");
-                sc.nextLine();
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                display(array);
+                if (array[j] > array[j+1]) {
+                    System.out.print("\t" + array[j] + " > " + array[j+1] + ", swap");
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }else if (array[j] < array[j+1]) {
+                    System.out.print("\t" + array[j] + " < " + array[j+1] + ", ok");
+                }else if (array[j] == array[j+1]) {
+                    System.out.print("\t" + array[j] + " = " + array[j+1] + ", ok");
+                }
+                System.out.println();
             }
-        } while (number < 0 || !isDecimal("" + number));
-        return (int) number;
-    }
-    
-    public static boolean isDecimal(String number) {
-        try {
-            //convert string into a decimal number
-            Double.parseDouble(number);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-            //if exception catched, it does not decimal number
+            System.out.println();
         }
     }
 }
